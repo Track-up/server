84cf0d5b65e9a100a20e74e952733e7b373518a8
Gerard <gerartph@gmail.com>
2023-10-29 16:32:53 +0100

    cors fuera

--- src/main/java/com/gimnsio/libreta/config/CorsConfig.java
+++ src/main/java/com/gimnsio/libreta/config/CorsConfig.java
@@ -1,18 +1,18 @@
-package com.gimnsio.libreta.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://trackupweb.vercel.app");
-    }
-
-}
+//package com.gimnsio.libreta.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://trackupweb.vercel.app");
+//    }
+//
+//}
