30fc59c69e0f2d6634d33374e3d79c71db22453f
GERARD MARTINEZ <gerartph@gmail.com>
2023-11-29 11:18:18 +0100

    newExercise - bodyparts

--- src/main/java/com/gimnsio/libreta/DTO/exercises/ExerciseNewDTO.java
+++ src/main/java/com/gimnsio/libreta/DTO/exercises/ExerciseNewDTO.java
@@ -1,8 +1,6 @@
 package com.gimnsio.libreta.DTO.exercises;
 
-import com.gimnsio.libreta.DTO.bodyPart.BodyPartIdDTO;
 import com.gimnsio.libreta.DTO.equipment.EquipmentIdDTO;
-import com.gimnsio.libreta.DTO.muscles.MusclePercentDTO;
 import com.gimnsio.libreta.DTO.muscles.MusclePercentIdDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,11 +14,6 @@ public class ExerciseNewDTO {
     private String name;
     private String image;
     private String description;
-
-
-    private List<BodyPartIdDTO> bodyParts;
-
     private EquipmentIdDTO equipment;
-
     private List<MusclePercentIdDTO> muscles;
 }
